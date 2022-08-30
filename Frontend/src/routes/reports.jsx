b35dc629b84b7614dea307e8aa57043469c41ac1
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2022-08-30 16:08:04 +0700

    update reports.jsx

--- Frontend/src/routes/reports.jsx
+++ Frontend/src/routes/reports.jsx
@@ -1,17 +1,31 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Card } from "react-bootstrap";
+import useWindowDimensions from "../hook/dimension";
 
 export default function Reports() {
+  const { width } = useWindowDimensions();
   const [type, setType] = useState("waiting");
   const [list, setList] = useState([]);
+
+  const status = {
+    waiting: "รอรับเรื่อง",
+    doing: "กำลังดำเนินการ",
+    finished: "ดำเนินการเสร็จสิ้น",
+  };
 
   async function fetchData() {
     await axios
       .get("http://127.0.0.1:5050/tasks?queryTaskStatus=" + type)
       .then((res) => {
         console.log(res.data);
-        setList(res.data);
+
+        var data = res.data;
+
+        data.forEach((value, index) => {
+          data[index].taskStatus = status[data[index].taskStatus];
+        });
+        setList(data);
       });
   }
 
@@ -35,11 +49,20 @@ export default function Reports() {
           <option value="finished">ดำเนินการแก้ไขเสร็จสิ้น</option>
           <option value="doing">กำลังดำเนินการ</option>
         </Form.Select>
-        {
-          list.length !== 0 ? list.map((value) => {
-            return <h1 key={value.id}>{value.taskName}</h1>
-          }) : null
-        }
+        {list.length !== 0
+          ? list.map((value) => {
+              return (
+                <Card key={value.id} className="mt-3">
+                  <Card.Img variant="top" src={value.taskPhotoIn} />
+                  <Card.Body>
+                    <Card.Title>{value.taskName}</Card.Title>
+                    <Card.Text>{value.taskLocation}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>{"สถานะ: " + value.taskStatus}</Card.Footer>
+                </Card>
+              );
+            })
+          : null}
       </Container>
     </div>
   );
